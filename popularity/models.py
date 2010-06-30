3c38c964ff61d2c76340b645ce3d7e8f907ace95
Brad Choate <brad@bradchoate.com>
2010-06-30 16:34:00 -0700

    Fixed syntax error and cache invalidation issues.

--- popularity/models.py
+++ popularity/models.py
@@ -20,21 +20,25 @@ def utility_user(user_info):
     return user
 
 
-def favorite(user, asset):
+def favorite(user, asset, group):
     token = typepad.client.token
     user = utility_user(user)
     fav = models.Favorite()
     fav.in_reply_to = asset.asset_ref
     user.favorites.post(fav)
+    signals.favorite_created.send(sender=favorite, instance=fav, parent=asset,
+        group=group)
     typepad.client.token = token
 
 
-def unfavorite(user, asset):
+def unfavorite(user, asset, group):
     token = typepad.client.token
     user = utility_user(user)
     fav = models.Favorite.get_by_user_asset(user.url_id, asset.url_id)
     try:
         fav.delete()
+        signals.favorite_deleted.send(sender=favorite, instance=fav,
+            parent=asset, group=group)
     except:
         pass
     typepad.client.token = token
@@ -42,8 +46,11 @@ def unfavorite(user, asset):
 
 if hasattr(settings, 'POPULAR_FAVORITE_COUNT'):
     def favorite_created(sender, instance, group=None, parent=None, **kwargs):
+        # we don't care about our own favoriting
+        if sender == favorite: return
+
         if parent.favorite_count + 1 == settings.POPULAR_FAVORITE_COUNT:
-            favorite(settings.POPULAR_USER, parent)
+            favorite(settings.POPULAR_USER, parent, group)
         else:
             # do we follow the user that made this favorite? if so, lets favorite it!
             typepad.client.batch_request()
@@ -51,12 +58,15 @@ if hasattr(settings, 'POPULAR_FAVORITE_COUNT'):
                 by_user=instance.author.url_id)
             typepad.client.complete_batch()
             if rels and rels.total_results > 0 and \
-                rels.entries[0].target.url_id = instance.author.url_id:
-                favorite(settings.POPULAR_USER, parent)
+                rels.entries[0].target.url_id == instance.author.url_id:
+                favorite(settings.POPULAR_USER, parent, group)
 
     def favorite_deleted(sender, instance, group=None, parent=None, **kwargs):
+        # we don't care about our own favoriting
+        if sender == unfavorite: return
+
         if parent.favorite_count == settings.POPULAR_FAVORITE_COUNT:
-            unfavorite(settings.POPULAR_USER, parent)
+            unfavorite(settings.POPULAR_USER, parent, group)
 
     signals.favorite_created.connect(favorite_created)
     signals.favorite_deleted.connect(favorite_deleted)
